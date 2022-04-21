0b91674f0cff7421fdd9dae5e61a3d91f7fcdf8d
ignacio745 <62523458+ignacio745@users.noreply.github.com>
2022-04-21 17:25:04 -0300

    Add files via upload

---
+++ ClaseEmail.py
@@ -0,0 +1,48 @@
+import re
+
+class Email:
+    __idCuenta = ""
+    __dominio = ""
+    __tipoDominio = ""
+    __contraseña = ""
+
+    def __init__(self, idCuenta="", dominio="", tipoDominio="", contraseña=""):
+        self.__idCuenta = idCuenta
+        self.__dominio = dominio
+        self.__tipoDominio = tipoDominio
+        self.__contraseña = contraseña
+
+    def retornaEmail(self):
+        email = self.__idCuenta + "@" + self.__dominio + "." + self.__tipoDominio
+        return email
+    
+    def getIdentificador(self):
+        return self.__idCuenta
+
+    def getDominio(self):
+        return self.__dominio
+
+    def cambiarContraseña(self, contraseña, nuevaContraseña):
+        band = False
+        if isinstance(nuevaContraseña, str):
+            if self.__contraseña == contraseña:
+                self.__contraseña = nuevaContraseña
+                band = True
+        return band
+
+    def crearCuenta(self, direccion:str, contraseña, antiguaContraseña=""):
+        band = False
+        if isinstance(direccion, str):
+            expresion = "[A-Za-z0-9\.\-\_]+@[a-z]+\.[a-z]{2,5}"
+            if re.match(expresion, direccion) and self.cambiarContraseña(antiguaContraseña, contraseña):
+                partes = direccion.split("@")
+                idCuenta = partes[0]
+                partes = partes[1].split(".")
+                dominio = partes[0]
+                tipoDominio = partes[1]
+                self.__idCuenta = idCuenta
+                self.__dominio = dominio
+                self.__tipoDominio = tipoDominio
+                band = True
+        return band
+
